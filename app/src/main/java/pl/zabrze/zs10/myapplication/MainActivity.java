539df00562db683f94b1109734991ccc64c5a18e
markup54 <marzenakupka@tlen.pl>
2023-10-25 16:07:32 +0200

    wiazanie widokow

--- app/src/main/java/pl/zabrze/zs10/myapplication/MainActivity.java
+++ app/src/main/java/pl/zabrze/zs10/myapplication/MainActivity.java
@@ -3,12 +3,32 @@ package pl.zabrze.zs10.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import pl.zabrze.zs10.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding; //nazwa od pliku xml
+    private Integer punkty =0 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        punkty++;
+                        binding.textView.setText(punkty.toString());
+                    }
+                }
+        );
+
     }
 }
